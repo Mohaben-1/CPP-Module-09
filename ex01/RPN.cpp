e73e7bacdec897c19223c4d1d41ef937b5e6f1d4
Mohaben-1 <mohammedwhb2001@gmail.com>
2025-09-24 18:52:09 +0100

    CPP Module 09

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -27,15 +27,56 @@ RPN::~RPN() {}
 
 void	RPN::calculate(const std::string& str)
 {
-	for (int i = 0; i < str.size(); i++)
+	for (int i = 0; i < static_cast<int>(str.size()); i++)
 	{
 		if (str[i] == ' ')
 			continue ;
 		else if (isdigit(str[i]))
 			container.push(str[i] - 48);
-		else if 
+		else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
 		{
-			
+			if (container.size() < 2)
+			{
+				std::cerr << "Error" << std::endl;
+				return ;
+			}
+			int	num1 = container.top();
+			container.pop();
+			int	num2 = container.top();
+			container.pop();
+			switch (str[i])
+			{
+				case '+':
+					container.push(num1 + num2);
+					break ;
+				case '-':
+					container.push(num2 - num1);
+					break ;
+				case '*':
+					container.push(num1 * num2);
+					break ;
+				case '/':
+					if (num1 == 0)
+					{
+						std::cerr << "Error" << std::endl;
+						return ;
+					}
+					container.push(num2 / num1);
+					break ;
+				default:
+					break ;
+			}
+		}
+		else
+		{
+			std::cerr << "Error" << std::endl;
+			return ;
 		}
 	}
+	if (container.size() != 1)
+	{
+		std::cerr << "Error" << std::endl;
+		return ;
+	}
+	std::cout << container.top() << std::endl;
 }
